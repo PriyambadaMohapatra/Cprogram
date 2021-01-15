f8168da777eaa6895d39db546644b8a6b2b736cc
Priyambada Mohapatra <76656517+PriyambadaMohapatra@users.noreply.github.com>
2021-01-15 21:13:42 +0530

    first commit

---
+++ 06_number_in_add.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int i,n;
+    printf("number system is:");
+    for (i=1;i<=n;i++)
+    {
+        printf("%d +",i);
+    }
+    return 0;
+}
---
+++ 06_square_of_no.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,c;
+    printf("number system is:");
+    for (i=1;i<=n;i++)
+    {
+        c=i*i;
+        printf("%d +",c);
+    }
+    return 0;
+}
